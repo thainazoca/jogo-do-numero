358ba7aa65453a84fc12181e8d29d0e974e28774
thainazoca <thaina.barbosa.santos@escola.pr.gov.br>
2025-08-07 07:44:22 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 
+const palpites = document.querySelector('.paplpites');
+const ultimoResultado = document.querySelector('.ultimoResultado');
+const baixoOuAlto = document.querySelector('.baixoOuAlto');
+const envioPalpite = document.querySelector('.envioPaplpite');
+const campoPalpite = document.querySelector('.campoPalpite');
+
+
+
+function verificarPalpite() {
+    const palpiteUsuario = Number (campoPalpite.value) ;
+    if (contagemPalpites === 1) {
+        palpites.textContent = "Palpites anteriores: ";
+    }
+
+    palpites.textContent += palpiteUsuario + " ";
+
+if (palpiteUsuario === numeroAleatorio) {
+    ultimoResultado.textContent = "Parabéns! voce acertou";
+
+}
